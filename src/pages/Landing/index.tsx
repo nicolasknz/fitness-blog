68f0e2ce95c768c87a74a10babb4de334279d067
nicolas <nicolasprodmac@gmail.com>
2020-10-31 23:08:38 -0300

    feat: posts are now being rendered on landing page dinamically

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,8 +4,24 @@ import Header from '../../components/header'
 import banner from '../../assets/images/banner.jpg'
 import Card from '../../components/card'
 
+import {posts} from '../../utils/posts'
 
 import * as Styled from './styles'
+
+interface CardProps {
+    thumbnail: {
+        image:string,
+        alt:string,
+     
+    },
+    description: string,
+    title: string,
+    author : {
+        image: string,
+        name: string
+    },
+    releaseDate: string
+}
 
 const Home = () => {
     return (
@@ -14,7 +30,29 @@ const Home = () => {
             <img src={banner} alt=""/>
 
             <main>
-           
+                {posts.map(({title,thumbnail,description,author,releaseDate}:CardProps,index) => {
+                        return (
+                            <Card 
+                            thumbnail={
+                                {
+                                    image:thumbnail.image,
+                                    alt: thumbnail.alt
+                                }
+                            }
+                            title={title}
+                            description={description}
+                            author={
+                                {
+                                    image:author.image,
+                                    name: author.name
+                                }
+                            }
+                            releaseDate={releaseDate}
+                            key={index}
+                         
+                        />
+                        )
+                })}
             </main>
         </Styled.LandingContainer>
     )
--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,8 +1,38 @@
-import girl from '../../assets/images/temp/girl.jpeg'
-import eGirl from '../../assets/images/temp/e-girl.jpg'
+import girl from '../assets/images/temp/girl.jpeg'
+import eGirl from '../assets/images/temp/e-girl.jpg'
 
-const posts = [
+export const posts = [
     {
+    thumbnail: {
+        image: girl,
+        alt: "Garota"
+    },
+
+    title: "13 melhores formas de comer manga com leite sem falecer hoje",
+    description:"Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Quem num gosta di mé.",
+    author:
+    {
+        image: eGirl,
+        name: "Fernanda Melo"
+    },
+    releaseDate: "25/10/2020"
+},
+{
+    thumbnail: {
+        image: girl,
+        alt: "Garota"
+    },
+
+    title: "13 melhores formas de comer manga com leite sem falecer hoje",
+    description:"Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Quem num gosta di mé.",
+    author:
+    {
+        image: eGirl,
+        name: "Fernanda Melo"
+    },
+    releaseDate: "25/10/2020"
+},
+{
     thumbnail: {
         image: girl,
         alt: "Garota"
