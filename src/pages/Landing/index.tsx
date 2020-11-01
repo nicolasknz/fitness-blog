44687fe97dbcd102aea1e7377cbc118f3c93acae
nicolas <nicolasprodmac@gmail.com>
2020-10-31 22:51:41 -0300

    feat: remove card from landing page and add it's information to post file

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -4,8 +4,6 @@ import Header from '../../components/header'
 import banner from '../../assets/images/banner.jpg'
 import Card from '../../components/card'
 
-import girl from '../../assets/images/temp/girl.jpeg'
-import eGirl from '../../assets/images/temp/e-girl.jpg'
 
 import * as Styled from './styles'
 
@@ -16,24 +14,7 @@ const Home = () => {
             <img src={banner} alt=""/>
 
             <main>
-                <Card 
-                    thumbnail={
-                        {
-                            image:girl,
-                            alt: "Garota"
-                        }
-                    }
-                    title="13 melhores formas de comer manga com leite sem falecer hoje"
-                    description="Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Quem num gosta di mé."
-                    author={
-                        {
-                            image:eGirl,
-                            name: "Fernanda Melo"
-                        }
-                    }
-                    releaseDate="25/10/2020"
-                 
-                />
+           
             </main>
         </Styled.LandingContainer>
     )
