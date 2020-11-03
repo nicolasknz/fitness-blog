bc24e67b3359659e34c07e2b4adc8febf6b27abb
nicolas <nicolasprodmac@gmail.com>
2020-11-02 23:32:50 -0300

    feat: added banner structure

---
+++ src/components/banner/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as Styles from './styles'
+
+const Banner = (src : string) => {
+    return (
+        <div>
+            <img src={src}/>
+        </div>
+    )
+}
+
+export default Banner
