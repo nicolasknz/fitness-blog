33e16ce84fdf91af3794a57d9149b4cc77f4f723
nicolas <nicolasprodmac@gmail.com>
2020-11-03 22:49:42 -0300

    feat: added links from router dom on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,27 +1,36 @@
-import React from 'react'
-import * as Styled from './styles'
-import InstagramIcon from '../../assets/images/icons/instagram-icon.svg'
-import FacebookIcon from '../../assets/images/icons/facebook-icon.svg'
+import React from "react";
+import * as Styled from "./styles";
+import InstagramIcon from "../../assets/images/icons/instagram-icon.svg";
+import FacebookIcon from "../../assets/images/icons/facebook-icon.svg";
+import { Link } from "react-router-dom";
 
 export const Header = () => {
-    return (
-      
-        <Styled.HeaderContent>
-           <Styled.Menu>
-                <div>HOME</div>
-                <div>CATEGORIAS</div>
-                <div>CONTATO</div>
-                <div>SOBRE</div>
-           </Styled.Menu>
+  return (
+    <Styled.HeaderContent>
+      <Styled.Menu>
+        <Link to="/">
+          {" "}
+          <div>HOME</div>
+        </Link>
+        <Link to="/categorias">
+          <div>CATEGORIAS</div>
+        </Link>
 
-           <Styled.SocialMedia>
-                <img src={InstagramIcon} alt="Icone Instagram"/>
-                <img src={FacebookIcon} alt="Icone Facebook"/>
-           </Styled.SocialMedia>
-        </Styled.HeaderContent>
-  
-      
-    )
-}
+        <Link to="/contato">
+          <div>CONTATO</div>
+        </Link>
+        <Link to="/sobre">
+          {" "}
+          <div>SOBRE</div>
+        </Link>
+      </Styled.Menu>
 
-export default Header
+      <Styled.SocialMedia>
+        <img src={InstagramIcon} alt="Icone Instagram" />
+        <img src={FacebookIcon} alt="Icone Facebook" />
+      </Styled.SocialMedia>
+    </Styled.HeaderContent>
+  );
+};
+
+export default Header;
