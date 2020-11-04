a2d886c59da3e2ced426b651f7b8c249a92aa132
nicolas <nicolasprodmac@gmail.com>
2020-11-04 20:03:13 -0300

    chore: organize sample posts

--- src/utils/posts/category1.ts
+++ src/utils/posts/category1.ts
@@ -1,5 +1,8 @@
 import girl from '../../assets/images/temp/girl.jpeg'
-import eGirl from '../../assets/images/temp/e-girl.jpg'
+import eGirl from '../../assets/images/temp/e_girl.jpg'
+
+import girl_fit from '../../assets/images/temp/girl-fit.jpeg'
+import guy from '../../assets/images/temp/guy.jpg'
 
 
 const category1 = [
@@ -20,34 +23,19 @@ const category1 = [
     },
     {
         thumbnail: {
-            image: girl,
+            image: girl_fit,
             alt: "Garota"
         },
 
-        title: "13 melhores formas de comer manga com leite sem falecer hoje",
-        description: "Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Quem num gosta di mé.",
+        title: "Aprenda agora como emagrecer 98kg por mês de graça",
+        description: "Mussum Ipsum, cacilds vidis litro abertis. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Praesent malesuada urna nisi, quis volutpat erat hendrerit non. Nam vulputate dapibus. Suco de cevadiss deixa as pessoas mais interessantis. A ordem dos tratores não altera o pão duris.",
         author:
         {
-            image: eGirl,
-            name: "Fernanda Melo"
+            image: guy,
+            name: "Marcio Lauro"
         },
-        releaseDate: "25/10/2020"
-    },
-    {
-        thumbnail: {
-            image: girl,
-            alt: "Garota"
-        },
-
-        title: "13 melhores formas de comer manga com leite sem falecer hoje",
-        description: "Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Quem num gosta di mé.",
-        author:
-        {
-            image: eGirl,
-            name: "Fernanda Melo"
-        },
-        releaseDate: "25/10/2020"
-    },
+        releaseDate: "30/11/2020"
+    }
 ]
 
 export default category1
