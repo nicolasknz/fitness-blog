a8010fde6d166e3e02087a6de85ede56bb73a661
nicolas <nicolasprodmac@gmail.com>
2020-11-04 20:33:24 -0300

    feat: added top bar structure

---
+++ src/components/top-bar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import * as Styled from './styles'
+
+const TopBar = () => {
+    return (
+        <Styled.Container>
+            Top Bar
+        </Styled.Container>
+    )
+}
+
+export default TopBar
