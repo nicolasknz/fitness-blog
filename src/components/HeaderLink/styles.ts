8c586efd3140b5770e656618a6785fd03c534ace
nicolas <nicolasprodmac@gmail.com>
2020-11-03 23:00:09 -0300

    feat: added header link style

--- src/components/HeaderLink/styles.ts
+++ src/components/HeaderLink/styles.ts
@@ -3,5 +3,11 @@ import {Link as LinkRouterDom} from 'react-router-dom'
 
 
 export const Link = styled(LinkRouterDom)`
-    
+ text-decoration:none;
+    &:hover {
+       div {
+           color:#fff;
+           background-color:#000;
+       }
+    }
 `
