69afc90f7be661ebe82eab6b5cd6eea80db2744e
nicolas <nicolasprodmac@gmail.com>
2020-11-04 23:20:40 -0300

    feat: added input and button handlers

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -1,14 +1,31 @@
-import React from "react";
-import * as Styled from "./styles";
+import React, {useState} from "react";
+
 import { AiOutlineSearch } from "react-icons/ai";
 
+import * as Styled from "./styles";
+
 const TopBar = () => {
+    const [search, setSearch] = useState("")
+    console.log(search)
+
+    const handleInput = (e:React.FormEvent<HTMLInputElement>) => {
+        setSearch(e.currentTarget.value)
+        console.log(search)
+    }
+
+    const handleClick = () => {
+        console.log(search)
+    }
+
   return (
     <Styled.Container>
       <Styled.SearchBarContainer>
-        <Styled.Input placeholder="O que você deseja saber?" />
-        <Styled.Button>
-      
+        <Styled.Input 
+            placeholder="O que você deseja saber?" 
+            onChange={handleInput}
+        />
+        <Styled.Button onClick={handleClick}>
+
             <AiOutlineSearch size="25px" color="#3d3b3b"/>
       
         </Styled.Button>
