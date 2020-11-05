11c631e2ffff5a0f088c9505ce908f0847246eb1
nicolas <nicolasprodmac@gmail.com>
2020-11-04 21:18:03 -0300

    feat: added button and styles on search bar

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import * as Styled from './styles'
+import React from "react";
+import * as Styled from "./styles";
+import { AiOutlineSearch } from "react-icons/ai";
 
 const TopBar = () => {
-    return (
-        <Styled.Container>
-            <Styled.Input placeholder="O que você deseja saber?" />
-        </Styled.Container>
-    )
-}
+  return (
+    <Styled.Container>
+      <Styled.SearchBarContainer>
+        <Styled.Input placeholder="O que você deseja saber?" />
+        <Styled.Button>
+      
+            <AiOutlineSearch size="25px" color="#3d3b3b"/>
+      
+        </Styled.Button>
+      </Styled.SearchBarContainer>
+    </Styled.Container>
+  );
+};
 
-export default TopBar
+export default TopBar;
--- src/components/top-bar/styles.ts
+++ src/components/top-bar/styles.ts
@@ -17,11 +17,39 @@ export const Input = styled.input`
     padding-left: 16px;
     height: 4rem;
     font-size: 1.2rem;
-    border:1px solid #9c98a6;
-    width:34rem;
+    /* border:1px solid #9c98a6; */
+    border:none;
+    width:87%;
 
     ::placeholder,
     ::-webkit-input-placeholder {
         color: #000;
   }
+  &:focus {
+        outline: none;
+       
+    }
 `
+export const SearchBarContainer = styled.div`
+    display:flex;
+    width:40rem;
+    border:1px solid #9c98a6;
+
+
+`
+export const Button = styled.button`
+    width:14%;
+    border:none;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    background-color:#eadb54;
+ 
+
+    &:hover {
+        background-color:#edd310;
+    }
+`
+
